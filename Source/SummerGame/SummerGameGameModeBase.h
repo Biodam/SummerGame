4bd70d35400053ca941d38f43809a85453b895ad
Fábio Damian <fabiodamian@gmail.com>
2020-01-15 13:47:19 -0300

    Initial commit

---
+++ Source/SummerGame/SummerGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SummerGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUMMERGAME_API ASummerGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
